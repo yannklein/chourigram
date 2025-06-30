4a7211102d058307a1bd37d75334557f1d8c124e
Yann Klein <yann.klein@me.com>
2025-06-30 09:27:26 +0900

    remove trailing slash from assetPrefix in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/chourigram',
-  assetPrefix: '/chourigram/',
+  assetPrefix: '/chourigram',
   eslint: {
     ignoreDuringBuilds: true,
   },
