30f230c97ec940948ae792cf0f400b04500302a6
Yann Klein <yann.klein@me.com>
2025-07-02 01:44:20 +0900

    add dev mode for url

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.IS_DEV === 'true';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/chourigram',
-  assetPrefix: '/chourigram',
+  basePath: isDev ? '' : '/chourigram',
+  assetPrefix: isDev ? '' : '/chourigram',
   eslint: {
     ignoreDuringBuilds: true,
   },
